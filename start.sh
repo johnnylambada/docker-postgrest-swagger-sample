1d56dc59181fa19fd6e5c6c13f7f841af8b2356f
John Lombardo <john@lombardos.org>
2020-11-24 13:47:42 -0800

    Make the JWT

--- start.sh
+++ start.sh
@@ -2,4 +2,11 @@
 
 rm -rf postgres/pgdata
 
-docker-compose up
+# Allow "tr" to process non-utf8 byte sequences
+export LC_CTYPE=C
+
+# read random bytes and keep only alphanumerics
+export TODO_SECRET=$(< /dev/urandom tr -dc A-Za-z0-9 | head -c32)
+echo TODO_SECRET=$TODO_SECRET
+
+docker-compose up --build --force-recreate
