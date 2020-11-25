deabe7cbeea497db70e7bfa8a8648253d94dec42
John Lombardo <john@lombardos.org>
2020-11-25 15:47:38 -0800

    The Authorization Bearer token is making it to the POST

--- start.sh
+++ start.sh
@@ -2,11 +2,7 @@
 
 rm -rf postgres/pgdata
 
-# Allow "tr" to process non-utf8 byte sequences
-export LC_CTYPE=C
-
-# read random bytes and keep only alphanumerics
-export TODO_SECRET=$(< /dev/urandom tr -dc A-Za-z0-9 | head -c32)
+export TODO_SECRET=$(LC_CTYPE=C < /dev/urandom tr -dc ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 | head -c32)
 echo TODO_SECRET=$TODO_SECRET
 
 docker-compose up --build --force-recreate
