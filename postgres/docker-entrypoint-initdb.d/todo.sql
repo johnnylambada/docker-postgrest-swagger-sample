40cd4611feffb6b49842d718e01e5b609e79046d
John Lombardo <john@lombardos.org>
2020-11-25 16:05:22 -0800

    Finish fixing

--- postgres/docker-entrypoint-initdb.d/todo.sql
+++ postgres/docker-entrypoint-initdb.d/todo.sql
@@ -15,3 +15,10 @@ grant select on api.todos to web_anon;
 
 create role authenticator noinherit login password 'mysecretpassword';
 grant web_anon to authenticator;
+
+create role todo_user nologin;
+grant todo_user to authenticator;
+
+grant usage on schema api to todo_user;
+grant all on api.todos to todo_user;
+grant usage, select on sequence api.todos_id_seq to todo_user;
